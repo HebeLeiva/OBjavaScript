fe74459a3a810198195926f5978a782ebc38c851
Hebe Leiva <hebe_leiva@yahoo.com.ar>
2022-10-15 18:06:43 -0300

    ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+const nombre = "Hebe";
+
+const edad = 49;
+
+const desarrollador = true;
+
+const nacimiento = new Date(1973, 3, 17);
+
+const libro = {
+    titulo: "El juego del angel",
+    autor: "Carlos Ruíz Zafón",
+    fecha: new Date(2008, 3, 17),
+    url: "https://www.planetadelibros.com.ar/libro-el-juego-del-angel/245188"
+}
+
+
