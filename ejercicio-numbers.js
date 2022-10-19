0966739f67f9e3186541f91953706e1bf07eaf8a
Hebe Leiva <hebe_leiva@yahoo.com.ar>
2022-10-19 16:49:54 -0300

    Ejercicios Numbers

---
+++ ejercicio-numbers.js
@@ -0,0 +1,22 @@
+let alturaCM = 159;
+
+let alturaMTS = (alturaCM / 100);
+
+let peso = 70.4;
+
+let alturaMTS_rounded = (Math.round(alturaMTS));
+
+let peso_rounded = (peso.toFixed(0));
+
+let max = Number.MAX_VALUE;
+
+let max1= max + 1
+
+if (resultado = max === max1) {
+    console.log("Son iguales")
+} else if (max1 > max){
+    console.log("max1 es mayor")
+} else {
+    console.log("max es mayor")
+}
+
