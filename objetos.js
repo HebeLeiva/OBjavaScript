17e2a936eaf18f306869d2281c50ab138f322ed3
Hebe Leiva <hebe_leiva@yahoo.com.ar>
2022-10-23 18:00:17 -0300

    ejercicio objetos.js

--- objetos.js
+++ objetos.js
@@ -1,12 +1,16 @@
-const datosPersonales = [{ nombre: "Hebe", apellido: "Leiva", edad: 47, altura: 1.6, eresDesarrollador: true }];
+const datosPersonales = { nombre: "Hebe", apellido: "Leiva", edad: 47, altura: 1.6, eresDesarrollador: true };
 
 const miEdad = datosPersonales.edad
 
-const datosVale = [{ nombre: "Valeria", apellido: "Espagnol", edad: 48,  altura: 1.68, eresDesarrollador: false }];
+const datosVale = [];
 
-const datosTere = [{ nombre: "Teresita", apellido: "Serra", edad: 49, altura: 1.69, eresDesarrollador: false }];
+const datosTere = [];
 
 
-const datosTodas = [...datosPersonales, ...datosTere, ...datosVale];
+const datosTodas = [
+    {...datosPersonales},
+    { nombre: "Valeria", apellido: "Espagnol", edad: 48,  altura: 1.68, eresDesarrollador: false },
+    { nombre: "Teresita", apellido: "Serra", edad: 49, altura: 1.69, eresDesarrollador: false }
+];
 
 const datosOrdenados = datosTodas.sort((a, b) => a.edad - b.edad);
