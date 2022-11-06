35ab39952dcdac296422d62e859254b5a32541f5
Hebe Leiva <hebe_leiva@yahoo.com.ar>
2022-11-06 16:52:38 -0300

    Ejercicio sesión 13

---
+++ sesion-13/index.js
@@ -0,0 +1,19 @@
+let nombre = "Hebe";
+
+let apellido = "Leiva";
+
+let estudiante = (`${nombre} ${apellido}`);
+
+let estudianteMayus = (estudiante.toUpperCase());
+
+let estudianteMinus = (estudiante.toLowerCase());
+
+let cantidadCaracteres = (estudiante.length);
+
+let primeraLetra = (nombre[0]);
+
+let ultimaLetra = (apellido[(apellido.length) - 1]);
+
+let sinEspacios = (`${nombre}${apellido}`);
+
+let existe = (estudiante.includes("Hebe"));
